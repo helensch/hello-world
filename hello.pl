18f8fff36c5790c2cc8a6d6fbaabe66c11bd3c35
Helen Schuilenburg <helens@ebi.ac.uk>
2017-11-15 13:57:35 +0000

    add date

--- hello.pl
+++ hello.pl
@@ -1,8 +1,8 @@
 #!/bin/env perl
-#
+# 2017-11-15
 use warnings;
 use strict;
 
 print "hello world\n";
-
+print "date is " . localtime() . "\n";
 
--- hello2.pl
+++ hello2.pl
@@ -1,8 +1,9 @@
 #!/bin/env perl
 #
+# 2017-11-15
 use warnings;
 use strict;
 
 print "hello world 2\n";
-
+print "data is " . localtime() . "\n";
 
